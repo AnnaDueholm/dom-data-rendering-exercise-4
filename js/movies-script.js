84e61a14f12ca1e1d2e87bc443d6ec2813ed727a
AnnaDueholm <eaa25alds@students.eaaa.dk>
2026-04-20 14:27:45 +0200

    opgave 17
    Filtering fortsættes og det indsættes at hvis søgefeltet ikke er tomt, og indeholder et match på film titel, så returneres en film

--- js/movies-script.js
+++ js/movies-script.js
@@ -105,6 +105,10 @@ if (selectedValue != "All") {
         return movie.genre === selectedValue;
     }); 
 }
+if (searchTerm !="" ) {
+    filteredMovies = filteredMovies.filter((movie)=> {
+    return movie.titel.toLowerCase().includes(searchTerm);    
+});
 
-
+}
 }
